0fd3070cea5bcb8dc0dd358b4c76b98132854679
Timothy-Harianja <timothy.harianja@gmail.com>
2020-01-06 14:38:32 +0700

    Updates

---
+++ precache-manifest.fe54aee182ce44e25a788a19d8213921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8effd2b0f119c406150481d3cffe9ba6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0f46085a7a78900256f0",
+    "url": "/monsters-rolodex/static/css/main.1bf77d99.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f46085a7a78900256f0",
+    "url": "/monsters-rolodex/static/js/main.f3fda749.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
